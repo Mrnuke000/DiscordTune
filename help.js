908b4df2184355a15d3d73c1f3c84a2247cc7074
Mrnuke <64882625+Mrnuke000@users.noreply.github.com>
2021-09-18 11:23:26 -0700

    Create help.js
    added help code to help depository

---
+++ help.js
@@ -0,0 +1,19 @@
+module.exports = {
+  'help': {
+    description: 'Shows the list of commands or help on specified command.',
+    format: 'help [command-name]'
+  },
+  'ping': {
+    description: 'Checks connectivity with discord\'s servers.',
+    format: 'ping'
+  },
+  'say': {
+    aliases: ['repeat'],
+    description: 'Repeats whatever is said.',
+    format: 'say <message>'
+  },
+  'play': {
+    description: 'plays music from youtube into a voice channel (still in development)',
+    format: 'play <youtubeURL/YoutubeVideo>'
+  }
+}
